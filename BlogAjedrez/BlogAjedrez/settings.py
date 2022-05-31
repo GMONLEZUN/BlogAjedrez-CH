b8207f7ea2288197c8c1cb8fa0a2786eb2ddb27b
gmonlezun <gabriel.monlezun@gmail.com>
2022-05-31 17:21:29 -0300

    [FIX] added db

--- BlogAjedrez/BlogAjedrez/settings.py
+++ BlogAjedrez/BlogAjedrez/settings.py
@@ -140,7 +140,6 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
